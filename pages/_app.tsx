3cc2206457e02fa783090a7747c0e3f669847c19
Vrishin Patel <vrishin_patel@outlook.com>
2022-10-18 07:11:31 +0000

    chore: add dependendies and clean up bloat

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import '@master/css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+	return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default App;
